66fd16ecd2a686cbeee9bfb8e2e0fa352886df3e
iaman786 <gupta.amandeep786@gmail.com>
2025-03-27 04:04:38 +0530

    tests added

---
+++ src/SimulatorAppTests/RobotReportTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using ToyRobotSimulation;
+using NUnit.Framework;
+
+namespace SimulatorAppTests
+{
+    public class RobotReportTests
+    {
+        [Test]
+        public void Report_ReturnsCorrectPositionAndFacing()
+        {
+            var table = new Table(5, 5);
+            var robot = new Robot(table);
+
+            robot.Place(2, 3, "EAST");
+            var report = robot.Report();
+            report.Should().Be("2,3,EAST");
+        }
+
+    }
+}
