fe2c83f18cface12c4dffc4bc0ea5bfc18c4a02d
iaman786 <gupta.amandeep786@gmail.com>
2025-03-27 03:20:57 +0530

    tests added

---
+++ tests/RobotMovementTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ToyRobotSimulationApp;
+
+namespace SimulatorAppTests
+{
+    internal class RobotMovementTests
+    {
+
+        [Test]
+        public void Move_ValidMove_UpdatesPosition()
+        {
+            var table = new Table(5, 5);
+            var robot = new Robot(table);
+
+            robot.Place(0, 0, "NORTH");
+            robot.Move();
+
+            robot.X.Should().Be(0);
+            robot.Y.Should().Be(1);
+        }
+
+        [Test]
+        public void Move_InvalidMove_DoesNotUpdatePosition()
+        {
+            var table = new Table(5, 5);
+            var robot = new Robot(table);
+
+            robot.Place(0, 5, "NORTH");
+            robot.Move();
+
+            robot.X.Should().Be(0);
+            robot.Y.Should().Be(5);// Robot remains at the edge
+        }
+    }
+}
---
+++ tests/RobotPlacementTests.cs
@@ -0,0 +1,31 @@
+﻿using ToyRobotSimulationApp;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace SimulatorAppTests
+{
+    public class RobotPlacementTests
+    {
+        [Test]
+        public void Place_ValidPosition_SetsPositionAndFacing()
+        {
+            var table = new Table(5, 5);
+            var robot = new Robot(table);
+
+            robot.Place(0, 0, "NORTH");
+            robot.X.Should().Be(0);
+            robot.Y.Should().Be(0);
+            robot.Facing.Should().Be("NORTH");
+        }
+
+        [Test]
+        public void Place_InvalidPosition_IgnoresPlacement()
+        {
+            var table = new Table(5, 5);
+            var robot = new Robot(table);
+
+            robot.Place(-1, 0, "NORTH");
+            robot.Facing.Should().BeNull();
+        }
+    }
+}
